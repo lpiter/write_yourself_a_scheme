d9bea43ed18bd3e2d68af113349e84c6e7d00c55
Piotr Łusakowski <revelation@o2.pl>
2015-07-20 19:52:23 +0200

    Ghc-modi suggested cosmetics + a cabal file.

--- src/Main.hs
+++ src/Main.hs
@@ -38,7 +38,7 @@ module Main where
      return $ String x
 
  parseBool :: Parser LispVal
- parseBool = try $ ((string "#t" >> return (Bool True)) <|> (string "#f" >> return (Bool False)))
+ parseBool = try $ (string "#t" >> return (Bool True)) <|> (string "#f" >> return (Bool False))
 
  parseAtom :: Parser LispVal
  parseAtom = do
@@ -46,40 +46,24 @@ module Main where
      rest <- many $ letter <|> digit <|> symbol
      return $ Atom (first:rest)
 
- parseNumber' :: Parser LispVal
- parseNumber' = do
-     digits <- many1 digit
-     return . Number . read $ digits
-
- parseNumber'' :: Parser LispVal
- parseNumber'' = many1 digit >>= return . Number . read
-
- parseNumber''' :: Parser LispVal
- parseNumber''' = liftM (Number . read) $ many1 digit
-
  parseNumber :: Parser LispVal
- parseNumber = do
-     x <- parseDecimal <|> parseDecimalWithRedix <|> parseOctal <|> parseHexadecimal <|> parseBinary
-     return x
+ parseNumber = parseDecimal <|> parseDecimalWithRedix <|> parseOctal <|> parseHexadecimal <|> parseBinary
      where
-         parseDecimal = many1 digit >>= return . Number . read
+         parseDecimal = liftM (Number . read) (many1 digit)
          parseDecimalWithRedix = try (string "#d") >> parseDecimal
+         parseOctal =       liftM (Number . fst . head . readOct) (try (string "#o") >> many1 octDigit)
+         parseHexadecimal = liftM (Number . fst . head . readHex) (try (string "#x") >> many1 hexDigit)
 
-         parseOctal = try (string "#o") >> many1 octDigit >>=
-             return . Number . fst . (\x -> readOct x !! 0)
-
-         parseHexadecimal = try (string "#x") >> many1 hexDigit >>=
-             return . Number . fst . (\x -> readHex x !! 0)
-
-         parseBinary = try (string "#b") >> many1 (oneOf "01") >>=
-             return . Number . (foldl (\z a -> a + 2 * z) 0) . (map $ toInteger . digitToInt)
+         parseBinary = liftM (Number . foldl (\z a -> a + 2 * z) 0 . map (toInteger . digitToInt)) readBinaryString
+            where
+                readBinaryString = try (string "#b") >> many1 (oneOf "01")
 
  parseFloat :: Parser LispVal
  parseFloat = do
      x <- many1 digit
      _ <- char '.'
      y <- many1 digit
-     return . Float . fst $ ((readFloat (x ++ "." ++ y)) !! 0)
+     return . Float . fst $ head $ readFloat (x ++ "." ++ y)
 
  parseCharacter :: Parser LispVal
  parseCharacter = do
@@ -90,11 +74,12 @@ module Main where
          -- character names are case-insensitive
          characterName = (ciString "space" >> return ' ')
                      <|> (ciString "newline" >> return '\n')
+            where
+                ciString = mapM (\c -> char (toLower c) <|> char (toUpper c)) -- case-insensitive string
          singleChar = do
              c <- anyChar
              notFollowedBy alphaNum
              return c
-         ciString = mapM (\c -> char (toLower c) <|> char (toUpper c)) -- case-insensitive string
 
  parseExpr :: Parser LispVal
  parseExpr = parseAtom
@@ -110,9 +95,9 @@ module Main where
      Right val -> show val
 
  main :: IO ()
- main = mapM readArg args >> return ()
+ main = void (mapM readArg args)
      where
          args = ["12.33", "12", "#\\NeWline", "#\\a", "#x123", "\"This \\n is \\\" a string\""]
-         readArg a = do
+         readArg a =
              putStrLn $ show a ++ " --> " ++ show (readExpr a)
 
